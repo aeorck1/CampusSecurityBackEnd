63e4857c8c671db4d3a275bb652504a8e26305ca
aeorck1 <lemuelgodwin2@gmail.com>
2025-05-30 15:09:52 +0100

    Update CORS settings and enhance .gitignore

--- campus_security_api/settings.py
+++ campus_security_api/settings.py
@@ -180,7 +180,7 @@ SPECTACULAR_SETTINGS = {
 
 #################################### CORS Settings ####################################
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
+    "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8080",
     "http://127.0.0.1:5500",
@@ -189,6 +189,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://campussecurityapi.onrender.com",
     "https://campussecuritybackend.onrender.com",
+    "https://campus-safe.vercel.app"
 ]
 #################################### End of CORS Settings ####################################
 
